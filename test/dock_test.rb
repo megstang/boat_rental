c1442f79d626c4eaa828648bb36e29c9d61f772d
Meghan Stang <mstang15@gmail.com>
2018-09-05 13:20:00 -0600

    finishes dock class and finishes iteration 2

--- test/dock_test.rb
+++ test/dock_test.rb
@@ -3,3 +3,24 @@ require 'minitest/pride'
 require './lib/boat'
 require './lib/dock'
 require './lib/boat'
+require './lib/renter'
+
+class DockTest <Minitest::Test
+  def test_it_exists
+    dock = Dock.new("The Rowing Dock", 3)
+
+    assert_instance_of Dock, dock
+  end
+
+  def test_dock_has_a_name
+    dock = Dock.new("The Rowing Dock", 3)
+
+    assert_equal "The Rowing Dock", dock.name
+  end
+
+  def test_dock_has_max_rental_time
+    dock = Dock.new("The Rowing Dock", 3)
+
+    assert_equal 3, dock.max_rental_time
+  end
+end
